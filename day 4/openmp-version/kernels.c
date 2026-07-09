9fe4d935d5f95b3d8b2d8164fb2f9ea5126e619b
acdc580 <acdc580@github.com>
2026-07-09 12:50:52 +0300

    corrected implementation for omp

--- day 4/openmp-version/kernels.c
+++ day 4/openmp-version/kernels.c
@@ -42,20 +42,15 @@ double dot(const int n, const double * restrict x, const double * restrict y)
 void matvec(const int n, const matrix_t *A, const double * restrict x,
             double * restrict y)
 {
-    /***** TODO *****/
     #pragma omp parallel for
     for (int i = 0; i < n; i++) {
-        y[i] = 0.0;
-    }
-
-    #pragma omp parallel for
-    for (int i = 0; i < n; i++) {
-        y[i] += A->diag[i] * x[i];
-    }
-
-    #pragma omp parallel for
-    for (int i = 0; i < n - 1; i++) {
-        y[i] += x[i + 1];
-        y[i + 1] += x[i];
+        // 1. Multiply the main diagonal (this applies to every row)
+        y[i] = A->diag[i] * x[i];
+        
+        // 2. Subtract the neighboring nodes ONLY if it is an interior node
+        if (i > 0 && i < n - 1) {
+            y[i] -= x[i - 1]; // sub-diagonal
+            y[i] -= x[i + 1]; // super-diagonal
+        }
     }
 }
